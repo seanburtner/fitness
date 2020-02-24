d7c0ba6bb63e33738374b058ad4c5d47ff92468e
Sean Burtner <seb4jw@virginia.edu>
2020-02-24 18:02:21 -0500

    route base url to login page

--- fitness_app/fit/src/app/app.module.ts
+++ fitness_app/fit/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoutinesComponent } from './routines/routines.component';
 import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -16,8 +17,9 @@ import { RouterModule } from '@angular/router';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    RouterModule.forRoot([
+    RouterModule.forRoot([ // order matters - make sure more specific ones come first
       { path: 'routines', component: RoutinesComponent },
+      { path: '', component: LoginComponent},
     ])
   ],
   providers: [],
