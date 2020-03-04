df2a3eb5dfe11b6fda89597347a625dae9008587
Colton Demetriou <coltonofvb@gmail.com>
2020-03-04 00:47:46 -0500

    I care

--- fitness_app/fit/src/app/login/login.component.ts
+++ fitness_app/fit/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  @ViewChild('email') email: ElementRef;
+  @ViewChild('password') password: ElementRef;
   constructor() { }
+
+  validateForm(email:String, password:String) {
+    if(email=="" && password==""){
+      window.alert("Please fill in your email and password");
+    }
+    else if(email=="" && password!=""){
+      this.password.nativeElement.value = "";
+      window.alert("Please enter your email.");
+    }
+    else if(email!="" && password==""){
+      this.email.nativeElement.value = "";
+      window.alert("Please enter your password.");
+    }
+    else{
+      let at = false;
+      for(let i=0;i<email.length;i++){
+        if(email.charAt(i)=='@'){
+          at = true;
+          break;
+        }
+      }
+      if(at==false){
+        window.alert("Please enter a valid email.");
+        this.email.nativeElement.value = "";
+        this.password.nativeElement.value = "";
+      }
+      else{
+        window.alert("Valid email and password ;)");
+        this.email.nativeElement.value = "";
+        this.password.nativeElement.value = "";
+      }
+    }
+  }
 
   ngOnInit(): void {
   }
